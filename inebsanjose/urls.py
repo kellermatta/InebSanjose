82548967dd36fc0b5bf99a2a15d4ed37e8251644
KELLER OBDULIO MATTA CALDERON <keller@MacBook-Pro-de-KELLER.local>
2015-01-29 15:48:26 -0600

    se agrego token

--- inebsanjose/urls.py
+++ inebsanjose/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.authtoken import views
 from rest_framework import routers
 from alumnos import views
 
